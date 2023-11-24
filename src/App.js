147051d09cd9fdd10dfac4afb58aafc153db001d
deepakbhardwajjp <deepakbhardwajjp@gmail.com>
2023-11-24 14:06:59 +0530

    ReactProj

--- src/App.js
+++ src/App.js
@@ -2,22 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const HelloReact=(name)=>{
+    alert("Hello ReactJS inside function" + name);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="button" value="Hello React !" onClick={()=>HelloReact("Deepak")} />
     </div>
   );
 }
